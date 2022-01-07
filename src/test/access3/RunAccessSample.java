c58498938a65f2014036e8ca57f1f48632d66740
wkdtj <wkdtj@192.168.219.103>
2022-01-07 15:52:18 +0900

    f

--- src/test/access3/RunAccessSample.java
+++ src/test/access3/RunAccessSample.java
@@ -4,6 +4,13 @@ public class RunAccessSample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		AccessSample a1 = new AccessSample();
+		a1.n1 = 100;
+		a1.setN(88);
+		
+		System.out.printf("a1이 참조하는 AccessSample 객체의 n1 필드: %d", a1.n1);
+		System.out.printf("a1이 참조하는 AccessSample 객체의 n2 필드: %d", a1.n2);
+		
 
 	}
 
